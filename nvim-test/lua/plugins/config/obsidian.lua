4915320c859991728acf8f9084765881e54cbed6
CHARARA97 <3432950747@qq.com>
2026-07-13 09:33:56 +0800

    obsidian workspace

--- nvim-test/lua/plugins/config/obsidian.lua
+++ nvim-test/lua/plugins/config/obsidian.lua
@@ -3,8 +3,8 @@ require( 'obsidian' ).setup(
                 workspaces =
                 {
                         {
-                                name = "CHARARA97",
-                                path = "~/Documents/Obsidian/CHARARA97",
+                                name = "CHARARA97的仓库",
+                                path = "~/Documents/Obsidian/CHARARA97的仓库",
                         },
                 },
                 frontmatter = { enabled = false },
